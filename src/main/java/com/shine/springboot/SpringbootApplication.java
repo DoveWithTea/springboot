31b6588748dfcf98dcd2d21cab2641120cf7c85d
Administrator <289886202@qq.com>
2022-09-01 14:07:15 +0800

    Changes

--- src/main/java/com/shine/springboot/SpringbootApplication.java
+++ src/main/java/com/shine/springboot/SpringbootApplication.java
@@ -1,13 +1,13 @@
 package com.shine.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@AutoConfiguration
 @SpringBootApplication
 public class SpringbootApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
-
 }
--- src/main/java/com/shine/springboot/controller/UserController.java
+++ src/main/java/com/shine/springboot/controller/UserController.java
@@ -1,10 +1,26 @@
 package com.shine.springboot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * @author :Shine
  * @date : 2022-08-26 16:10
  */
+@Controller
 public class UserController {
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @ResponseBody
+    @RequestMapping("/index")
+    public String Index(){
+        return "Welcom HomePage";
+    }
 }
 /**
  * @BelongsProject: springboot
